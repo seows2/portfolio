7330e1733436bb4da49b6011dbbf93d802cf78eb
SeoWS <seows2@naver.com>
2020-12-17 19:05:08 +0900

    :sparkles: header up/down casing

---
+++ js/index.js
@@ -0,0 +1,26 @@
+export default function pageManager() {
+    let last = 0
+    const threshold = 200
+    const navigation = document.getElementById("nav-js")
+
+    const casing = () => {
+        const position = window.pageYOffset
+        const up = position < last
+        if(up || last === 0){
+            navigation.style.transform = "translateY(0)"
+        } else if (position > threshold){
+            navigation.style.transform = "translateY(-100%)"
+        }
+        last = position
+    }
+
+    const init = () => {
+        window.addEventListener("scroll", casing)
+    }
+
+    init()
+}
+
+window.onload = () =>{
+    new pageManager()
+}
