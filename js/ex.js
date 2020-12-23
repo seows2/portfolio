f74ec6ca7620bd752bbf48d7d40cfeed19988f8d
SeoWS <seows2@naver.com>
2020-12-23 18:59:56 +0900

    해상도에 따른 이미지 크기, 위치  수정 codePen참조

--- js/ex.js
+++ js/ex.js
@@ -3,9 +3,15 @@ export default class pageManager {
         this.menu = document.querySelector("#menu")
         this.navigation = document.querySelector("#nav-js")
         this.last = 0
+        this.cards = document.querySelectorAll('.image');
+        this.container = document.querySelector('.cards');
+        this.boxes = document.querySelectorAll('.box');
 
         this.menu.addEventListener("click", this.scrolling)
         window.addEventListener("scroll",this.casing)
+        window.addEventListener("resize",this.calculate)
+
+        this.calculate()
     }
 
     casing = () => {   
@@ -13,6 +19,7 @@ export default class pageManager {
         const threshold = 200
         const last = this.last
         const up = position < last
+        console.log(position);
         if(up || last === 0){
             this.navigation.style.transform = "translateY(0)"
         } else if (position > threshold){
@@ -25,7 +32,7 @@ export default class pageManager {
         const {nodeName} = e.target
         const {id} = e.target
         if(nodeName !== "A") return
-
+        console.log(e.target);
         if(id === "about-link"){
             this.scrollTo(0, 1000)
         } else if(id === "work-link"){
@@ -44,7 +51,7 @@ export default class pageManager {
             let distLeft = Math.abs(document.documentElement.scrollTop - destY)
             let moveBy = Math.min(perTick, distLeft)
             document.documentElement.scrollTop += moveBy * direction
-            if(distLeft > 0){
+            if(distLeft > 10){
                 timerId = setTimeout(doTick, 2)
             }
         }
@@ -52,6 +59,67 @@ export default class pageManager {
         doTick()
     }
 
+    calculate = () => {
+        this.iterate(this.boxes, 93.8, false);
+
+        let variable = window.innerWidth;
+        let factor = 4 / 3;
+        let number = 2;
+        let description = 148;
+        let percentage = 0.355;
+        let margin = 80;
+        let additional = 140;
+
+        if(variable > 1920){
+            variable = 1920
+        }
+
+        const calculation = (variable * percentage * factor - description);
+        const containerHeight = (calculation + description + margin) * number + additional;
+        for (let i = 0; i < this.cards.length; i++) {
+            const card = this.cards[i];
+            card.style.height = `${calculation}px`
+        }
+        if(this.container){
+            this.container.style.height = `${containerHeight}px`
+        }
+
+    }
+
+    iterate(properties, value, should = false, full = true) {
+        for (var i = 0; i < properties.length; i++) {
+          const proof = properties[i];
+          
+          if (window.innerWidth > 1920) {
+            proof.style.marginLeft = '-' + ((window.innerWidth - 1920) / 2 + (19.2 * 12.4)) + 'px';
+            proof.style.width = ((window.innerWidth - 1920) / 2 + (19.2 * value)) + 'px';
+  
+            if (value == 100) {
+              proof.style.width = '100vw';
+            }
+  
+            if (should == true) {
+              proof.style.marginLeft = '0px';
+              proof.style.width = ((window.innerWidth - 1920) / 2 + (19.2 * 87.6)) + 'px';
+            }
+          } else if (window.innerWidth < 1300) {
+            proof.style.marginLeft = '-6.2vw';
+            proof.style.width = '100vw';
+          } else if (window.innerWidth < 1920) {
+            proof.style.marginLeft = '-12.4vw';
+            proof.style.width = 93.9 + 'vw';
+  
+            if (value == 100) {
+              proof.style.width = '100vw';
+            }
+  
+            if (should == true) {
+              proof.style.marginLeft = '0px';
+              proof.style.width = 87.6 + 'vw';
+            }
+          }
+        }
+    }
 }
 
 new pageManager()
